0f5c0c014160c2600e0adf26765bb3122d333ea6
xIceArcher <xicearcher@gmail.com>
2022-03-08 23:48:37 +0800

    Fix Instagram mentions regex

--- instagram/consts.go
+++ instagram/consts.go
@@ -2,5 +2,5 @@ package instagram
 
 import "regexp"
 
-var HashtagRegex = regexp.MustCompile(`(?:[#|＃])[\p{L}\p{M}\p{N}][\p{L}\p{M}\p{N}_]*`)
-var MentionRegex = regexp.MustCompile(`(?:[@|＠])[\p{L}\p{M}][\p{L}\p{M}\p{N}_]*`)
+var HashtagRegex = regexp.MustCompile(`(?:[#|＃])[\p{L}\p{M}\p{N}][\p{L}\p{M}\p{N}_]+`)
+var MentionRegex = regexp.MustCompile(`(?:[@|＠])[A-Za-z0-9\._]+`)
