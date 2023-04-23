c69660345e89b5b1279e44812f374fd5e1409e87
xIceArcher <xicearcher@gmail.com>
2023-04-23 12:11:20 +0800

    Update message max bytes from 8MB to 25MB

--- discord/utils.go
+++ discord/utils.go
@@ -22,7 +22,7 @@ func GetMessageMaxBytes(boostTier discordgo.PremiumTier) int64 {
 	case discordgo.PremiumTier2:
 		availableBytes = 50 * megaByte
 	default:
-		availableBytes = 8 * megaByte
+		availableBytes = 25 * megaByte
 	}
 
 	return int64(availableBytes - slackBytes)
