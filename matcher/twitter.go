0c54ea25516c5ab710d96812f13583e1f0a48bfc
xIceArcher <xicearcher@gmail.com>
2025-03-06 20:26:51 +0800

    Improve responsiveness of Twitter matcher

--- matcher/twitter.go
+++ matcher/twitter.go
@@ -24,19 +24,6 @@ func NewTwitterPostMatcher(cfg *config.Config, s *discord.Session) (Matcher, err
 }
 
 func (m *TwitterPostMatcher) Handle(ctx context.Context, s *discord.MessageSession, matches []string) {
-	time.Sleep(5 * time.Second)
-
-	existingEmbeds, err := s.GetMessageEmbeds()
-	if err != nil {
-		s.Logger.With(zap.Error(err)).Warn("Failed to get message embeds")
-		return
-	}
-
-	// TODO: Find some way to match tweets to existing embeds
-	if len(matches) > 1 && len(existingEmbeds) > 0 {
-		return
-	}
-
 	tweetID := matches[0]
 
 	logger := s.Logger.With(
@@ -49,11 +36,9 @@ func (m *TwitterPostMatcher) Handle(ctx context.Context, s *discord.MessageSessi
 		return
 	}
 
-	if !isDiscordMainEmbedCorrect(tweet, existingEmbeds) {
-		s.SendEmbeds(tweet.GetEmbeds())
-	}
+	m.handleTweetMainEmbed(ctx, s, matches, tweet)
 
-	if tweet.HasVideos() && (len(existingEmbeds) == 0 || existingEmbeds[0].Video == nil) {
+	if tweet.HasVideos() {
 		for _, video := range tweet.Videos() {
 			if video.Type == twitter.MediaTypeGIF && strings.HasSuffix(video.URL, ".mp4") {
 				s.SendMP4URLAsGIF(video.URL, s.Message.ID)
@@ -64,9 +49,38 @@ func (m *TwitterPostMatcher) Handle(ctx context.Context, s *discord.MessageSessi
 	}
 }
 
+func (m *TwitterPostMatcher) handleTweetMainEmbed(_ context.Context, s *discord.MessageSession, matches []string, tweet *twitter.Tweet) {
+	var existingEmbeds discord.UpdatableMessageEmbeds
+	var err error
+
+	if isDiscordMainEmbedPossiblyCorrect(tweet) {
+		s.Logger.With(
+			zap.String("tweetID", tweet.ID),
+		).Info("Tweet is possibly embeddable, waiting...")
+
+		time.Sleep(3 * time.Second)
+
+		existingEmbeds, err = s.GetMessageEmbeds()
+		if err != nil {
+			s.Logger.With(zap.Error(err)).Warn("Failed to get message embeds")
+			return
+		}
+
+		// TODO: Find some way to match tweets to existing embeds
+		if len(matches) > 1 && len(existingEmbeds) > 0 {
+			return
+		}
+
+		if isDiscordMainEmbedCorrect(tweet, existingEmbeds) {
+			return
+		}
+	}
+
+	s.SendEmbeds(tweet.GetEmbeds())
+}
+
 func isDiscordMainEmbedCorrect(tweet *twitter.Tweet, existingEmbeds discord.UpdatableMessageEmbeds) bool {
-	// Discord can't embed polls
-	if tweet.Poll != nil {
+	if !isDiscordMainEmbedPossiblyCorrect(tweet) {
 		return false
 	}
 
@@ -80,6 +94,34 @@ func isDiscordMainEmbedCorrect(tweet *twitter.Tweet, existingEmbeds discord.Upda
 		return false
 	}
 
+	// Additional checks for tweets with photos
+	if tweet.HasPhotos() {
+		// The embed is missing the photo(s)
+		if existingEmbeds[0].Image == nil {
+			return false
+		}
+
+		// More than one photo case filtered out by isNotEmbeddableByDiscord
+
+		// First media is video or GIF case filtered out by isNotEmbeddableByDiscord
+
+		// At this point, we know:
+		// (1) The existing embed has photos
+		// (2) The tweet only has one photo (and potentially videos in some order)
+		// (3) The first media of this tweet is not a video or a GIF, hence is a photo
+		// (2) and (3) imply that the tweet's first media is the only photo
+		// Combied with (1), this means that the tweet's only photo is correctly embedded
+	}
+
+	return true
+}
+
+func isDiscordMainEmbedPossiblyCorrect(tweet *twitter.Tweet) bool {
+	// Discord can't embed polls
+	if tweet.Poll != nil {
+		return false
+	}
+
 	// Discord can't embed alt texts
 	for _, media := range tweet.Medias {
 		if media.AltText != "" {
@@ -89,28 +131,24 @@ func isDiscordMainEmbedCorrect(tweet *twitter.Tweet, existingEmbeds discord.Upda
 
 	// Additional checks for tweets with photos
 	if tweet.HasPhotos() {
-		// The embed is missing the photo(s)
-		if existingEmbeds[0].Image == nil {
-			return false
-		}
-
 		// Discord can't embed more than one photo
 		if len(tweet.Photos()) > 1 {
 			return false
 		}
 
-		// If the first media is a video, then Discord will wrongly embed it as a photo, and will not embed the rest of the photos
-		if tweet.Medias[0].Type == twitter.MediaTypeVideo {
+		// If the first media is a video or a GIF (and the tweet has other photos)
+		// Then Discord will wrongly embed it as a photo, and will not embed the rest of the photos
+		if tweet.Medias[0].Type == twitter.MediaTypeVideo || tweet.Medias[0].Type == twitter.MediaTypeGIF {
 			return false
 		}
-
-		// At this point, we know:
-		// (1) The existing embed has a photo
-		// (2) The tweet only has one photo (and potentially videos in some order)
-		// (3) The first media of this tweet is a photo
-		// (2) and (3) imply that the tweet's first media is the only photo
-		// Combied with (1), this means that the tweet's only photo is correctly embedded
 	}
+
+	// Discord might have a chance here
+	// Possible cases
+	// 0 photo case
+	// 1 photo case
+	// 1 video or GIF case (don't need to send the main embed if Discord sends it, we just need to send the video or GIF)
+	// First media is photo, with everything else being videos or GIFs (same as above case)
 
 	return true
 }
